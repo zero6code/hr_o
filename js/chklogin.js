15df894fe85de60679f57d196c0a713bba9afa1c
zero6code <nattawut.k06@gmail.com>
2022-07-21 02:02:10 +0700

    update

--- js/chklogin.js
+++ js/chklogin.js
@@ -5,20 +5,20 @@
             '<!-- Page Content -->'+
             '<div class="container">'+
                 '<div class="row">'+
-                    '<div class="col-md-4">'+//Content ขวามือ
+                    '<div class="col-12 col-sm-12 col-md-8 col-lg-6 col-xl-5 mx-auto">'+//Content ขวามือ
                         '<div class="row">'+
-                            "<form class='myboxshadow' style='background-color:white;'>"+
+                            "<form class='card shadow mt-5' style='background-color:white; padding: 2vh 2vw 2vh;'>"+
                                 "<div class='row'>"+
-                                    "<div class='col-md-6 col-md-offset-3'>"+
+                                    "<div class='col-6 col-md-6 col-md-offset-3 ' style='margin: auto;'>"+
                                         "<img src='img/logo_ssr.png' class='img-responsive' style='width:100%;'>"+
                                     "</div>"+
                                 "</div>"+
-                                "<h2 class='form-signin-heading'>HR@SSRH ระบบสารสนเทศเพื่อการบริหารทรัพยากรบุคคล</h2>"+
+                                "<h2 class='form-signin-heading' style='text-align: center; padding-top: 2vh; padding-bottom: 2vh;'>กลุ่มงานทรัพยากรบุคคล</h2>"+
                                 "<label  class='sr-only'>Username</label>"+
-                                "<input type='text' class='form-control' placeholder='Username' style='background-color:#FFFFE0;border-radius:0px;'>"+
+                                "<input type='text' class='form-control' placeholder='Username' style='height: 5vh;font-size: 20px;'>"+
                                 "<label  class='sr-only'>Password</label>"+
-                                "<input type='password'  class='form-control' placeholder='Password' style='background-color:#FFFFE0;border-radius:0px;'>"+
-                                '<div class="funkyradio">'+
+                                "<input type='password'  class='form-control' placeholder='Password' style='height: 5vh; font-size: 20px;'>"+
+                                '<div class="funkyradio" style="margin-top:3vh;">'+
                                     '<div class="funkyradio-success" style="padding:0; margin:0;">'+
                                         '<input type="radio" name="logintype" id="typeuser" value="typeuser" checked />'+
                                         '<label for="typeuser" style="padding:0; margin:0;">ผู้ใช้งานทั่วไป(user)</label>'+
@@ -29,10 +29,9 @@
                                     '</div>'+
                                 '</div>'+
                                 '<div class="row">'+
-                                    "<div class='col-xs-4 col-xs-offset-3 col-sm-2 col-sm-offset-1 col-md-2 col-md-offset-5'>"+
+                                    "<div class='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12'>"+
                                       "<button class='btn btn-primary' style='"+
-                                        "border-radius:30px;"+
-                                        "box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);"+
+                                        "width:100%; height: 5vh; margin-top:3vh; margin-bottom:3vh;"+
                                         "'>เข้าสู่ระบบ</button>"+
                                     '</div>'+
                                 '</div>'+
@@ -47,7 +46,7 @@
     $(document).ready(function($){
         $(document.body).append(html_layout());
         var content = $(document.body).find('div.container'),
-            contentRight = content.find('.row > .col-md-4'),
+            contentRight = content.find('.row > .col-12.col-sm-12.col-md-8.col-lg-6.col-xl-5.mx-auto'),
             //contentLeft = content.find('.row > .col-md-8'),
             staffLoginFrm = contentRight.find('div:nth-child(1) >form');
             
